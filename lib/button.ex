32d56d76125c616a9a1eee1c023ae6f6bd70b3a6
De Wet Blomerus <dewetblomerus@gmail.com>
2025-01-26 12:07:59 -0500

    mix new button --sup

---
+++ lib/button.ex
@@ -0,0 +1,18 @@
+defmodule Button do
+  @moduledoc """
+  Documentation for `Button`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Button.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
